71f5082984f8fea4fdd4939f0d26a7040b82b087
geogardow <85602062+geogardow@users.noreply.github.com>
2022-08-30 03:41:18 -0600

    Interface Finalized

--- interface.py
+++ interface.py
@@ -1,19 +1,21 @@
 from asyncio import events
+from asyncio.windows_events import NULL
 from distutils.cmd import Command
 from tkinter import *
 from tkinter import ttk
-from os import *
-from turtle import bgcolor, right
-from winreg import REG_DWORD_BIG_ENDIAN
+import os
 import tkinter.font as tkfont
+from tkinter import filedialog
 
 fnt=("Arial",10)
+filename = ""
 SPACING     =   5
 WIDTH       =   1200
 HEIGHT      =   700
 TOOLBWIDTH  =   50
 MENUHEIGHT  =   30
 FOLDERWIDTH =   200
+CONHEIGHT   =   230
 NUMBERSWIDTH=   50
 BUTTONSEP   =   0
 BUTTONGRID  =   TOOLBWIDTH+BUTTONSEP
@@ -28,27 +30,28 @@ REDBG       =   "#d61425"
 
 class Interface:
     def __init__(self):
-        ########################## MAIN PANEL ###########################
-        
-        
+        ########################## MAIN PANEL ###########################             
         self.ventana=Tk()
-        self.ventana.title("Computines Compiler")
+        self.ventana.title("TagaPlate")
         self.ventana.minsize(WIDTH,HEIGHT) 
         self.ventana.resizable(width=NO, height=NO)
         self.ventana.overrideredirect(True)
 
         def cargar_img(nombre):
-            ruta=path.join("assets",nombre) #crea acceso a la ruta assets dentro de la carpeta del programa
+            ruta=os.path.join("assets",nombre) #crea acceso a la ruta assets dentro de la carpeta del programa
             img=PhotoImage(file=ruta) #importa la imagen
             return img #retorna la imagen 
 
-        mainPanel = Canvas(self.ventana, background=BACKGROUND, width=WIDTH-TOOLBWIDTH-FOLDERWIDTH, height=HEIGHT-MENUHEIGHT, highlightthickness=0)
+        mainPanel = Canvas(self.ventana, background=BACKGROUND, width=WIDTH-TOOLBWIDTH-FOLDERWIDTH, height=HEIGHT-MENUHEIGHT-CONHEIGHT, highlightthickness=0)
         mainPanel.place(x=TOOLBWIDTH+FOLDERWIDTH+NUMBERSWIDTH,y=MENUHEIGHT)
+
+        consolePanel = Canvas(self.ventana, background=BACKGROUND, width=WIDTH-FOLDERWIDTH, height=CONHEIGHT, highlightthickness=0)
+        consolePanel.place(x=TOOLBWIDTH+FOLDERWIDTH,y=HEIGHT-CONHEIGHT)
 
         folderbar = Canvas(self.ventana, background=FOLDERBAR, width=FOLDERWIDTH, height=HEIGHT-MENUHEIGHT, highlightthickness=0)
         folderbar.place(x=TOOLBWIDTH,y=MENUHEIGHT)
 
-        numBar = Canvas(self.ventana, background=BACKGROUND, width=NUMBERSWIDTH, height=HEIGHT-MENUHEIGHT, highlightthickness=0)
+        numBar = Canvas(self.ventana, background=BACKGROUND, width=NUMBERSWIDTH, height=HEIGHT-MENUHEIGHT-CONHEIGHT, highlightthickness=0)
         numBar.place(x=TOOLBWIDTH+FOLDERWIDTH,y=MENUHEIGHT) 
 
         menubar = Canvas(self.ventana, background=MENUBAR, width=WIDTH, height=MENUHEIGHT, highlightthickness=0)
@@ -74,23 +77,78 @@ class Interface:
 
         menubar.bind('<Button-1>', get_pos)
 
-        openedFile=Label(self.ventana, font=fnt, fg="#dedede", bg=MENUBAR, text="Compilador Computines")
+        openedFile=Label(self.ventana, font=fnt, fg="#dedede", bg=MENUBAR, text="TagaPlate")
         openedFile.place(x=WIDTH/2,y=TOOLBWIDTH/4,anchor="center")
-
+        
         ########################## TREE DIRECTORY ###########################
-        yscrollTree = Scrollbar(folderbar, orient="vertical")
-        xscrollTree = Scrollbar(folderbar, orient="horizontal")
-        yscrollTree.place(x=FOLDERWIDTH, y=0, anchor=NE, height=HEIGHT-MENUHEIGHT-15)
-        xscrollTree.place(x=0, y=HEIGHT-MENUHEIGHT, anchor=SW, width=FOLDERWIDTH)
-
-        directory='/Documentos/TEC/'
+        directory=os.getcwd()
         treeFrame=Frame(folderbar, bg=FOLDERBAR, height=HEIGHT-MENUHEIGHT-15, width=FOLDERWIDTH-15)
+
         treeStyle = ttk.Style(treeFrame)
         treeStyle.theme_use("alt")
         treeStyle.configure("Treeview", background=FOLDERBAR, foreground="#dedede", fieldbackground=FOLDERBAR)
         treeStyle.configure("Treeview", highlightthickness=0, bd=0, font=('Arial', 10))
         treeStyle.layout("Treeview", [('Treeview.treearea', {'sticky': 'nswe'})])
         treeStyle.map("Treeview", background = [('selected', LIGHTERBG)])
+
+        scrollStyle = ttk.Style(folderbar)
+        scrollStyle.element_create('Horizontal.Scrollbar.trough', 'from', 'default')
+        scrollStyle.element_create('Horizontal.Scrollbar.thumb', 'from', 'default')
+        scrollStyle.element_create('Horizontal.Scrollbar.leftarrow', 'from', 'default')
+        scrollStyle.element_create('Horizontal.Scrollbar.rightarrow', 'from', 'default')
+        scrollStyle.element_create('Horizontal.Scrollbar.grip', 'from', 'default')
+        scrollStyle.layout('Horizontal.TScrollbar',
+            [
+                ('Horizontal.Scrollbar.trough', {'children':
+                    [('Horizontal.Scrollbar.leftarrow', {'side': 'left', 'sticky': ''}),
+                    ('Horizontal.Scrollbar.rightarrow', {'side': 'right', 'sticky': ''}),
+                    ('Horizontal.Scrollbar.thumb', {'unit': '1', 'children':
+                        [('Horizontal.Scrollbar.grip', {'sticky': ''})],
+                    'sticky': 'nswe'})],
+                    'sticky': 'we'}),
+            ])
+        scrollStyle.layout('Horizontal.TScrollbar', [('Horizontal.Scrollbar.trough', 
+        {'children': [('Horizontal.Scrollbar.thumb', {'expand': '1', 'sticky': 'nswe'})], 'sticky': 'ew'})])
+        scrollStyle.configure('Horizontal.TScrollbar',
+            troughcolor=FOLDERBAR,
+            background=FOLDERBAR,
+            arrowcolor=LIGHTERBG,
+            gripcolor="white",
+            thumbcolor="white")
+        scrollStyle.map('Horizontal.TScrollbar',
+            background=[('pressed', '!disabled', LIGHTERBG), ('active', TOOLBAR)])
+        
+        scrollStyle.element_create('Vertical.Scrollbar.trough', 'from', 'default')
+        scrollStyle.element_create('Vertical.Scrollbar.thumb', 'from', 'default')
+        scrollStyle.element_create('Vertical.Scrollbar.leftarrow', 'from', 'default')
+        scrollStyle.element_create('Vertical.Scrollbar.rightarrow', 'from', 'default')
+        scrollStyle.element_create('Vertical.Scrollbar.grip', 'from', 'default')
+        scrollStyle.layout('Vertical.TScrollbar',
+            [
+                ('Vertical.Scrollbar.trough', {'children':
+                    [('Vertical.Scrollbar.leftarrow', {'side': 'left', 'sticky': ''}),
+                    ('Vertical.Scrollbar.rightarrow', {'side': 'right', 'sticky': ''}),
+                    ('Vertical.Scrollbar.thumb', {'unit': '1', 'children':
+                        [('Vertical.Scrollbar.grip', {'sticky': ''})],
+                    'sticky': 'nswe'})],
+                    'sticky': 'we'}),
+            ])
+        scrollStyle.layout('Vertical.TScrollbar', [('Vertical.Scrollbar.trough', 
+        {'children': [('Vertical.Scrollbar.thumb', {'expand': '1', 'sticky': 'nswe'})], 'sticky': 'ns'})])
+        scrollStyle.configure('Vertical.TScrollbar',
+            troughcolor=FOLDERBAR,
+            background=FOLDERBAR,
+            arrowcolor=LIGHTERBG,
+            gripcolor="white",
+            thumbcolor="white")
+        scrollStyle.map('Vertical.TScrollbar',
+            background=[('pressed', '!disabled', LIGHTERBG), ('active', TOOLBAR)])
+
+        yscrollTree = ttk.Scrollbar(folderbar, orient="vertical")
+        xscrollTree = ttk.Scrollbar(folderbar, orient="horizontal")
+        yscrollTree.place(x=FOLDERWIDTH+1, y=-1, anchor=NE, height=HEIGHT-MENUHEIGHT-12)
+        xscrollTree.place(x=-1, y=HEIGHT-MENUHEIGHT+1, anchor=SW, width=FOLDERWIDTH+2)
+
         def fixed_map(option):
             return [elm for elm in treeStyle.map('Treeview', query_opt=option) if
             elm[:2] != ('!disabled', '!selected')]
@@ -100,13 +158,13 @@ class Interface:
         folderTree=ttk.Treeview(treeFrame, show='tree', yscrollcommand=yscrollTree.set,  xscrollcommand=xscrollTree.set)
         folderTree.heading('#0',text='directory:'+ directory,anchor='w')
         folderTree.heading('#0', text='Dir：'+directory, anchor='w')
-        directory=path.abspath(directory)
-        node=folderTree.insert('','end', text=path, open=True)
+        directory=os.path.abspath(directory)
+        node=folderTree.insert('','end', text=os.path, open=True)
         def traverse_dir(parent, directory):
-            for d in listdir(directory):
-                full_path=path.join(directory,d)
-                isdir = path.isdir(full_path)
-                id=folderTree.insert(parent,'end',text=d,open=False)
+            for d in os.listdir(directory):
+                full_path=os.path.join(directory,d)
+                isdir = os.path.isdir(full_path)
+                id=folderTree.insert(parent,'end',text=d,open=False, tags=tuple(map(str, full_path.split('%\%'))))
                 if isdir:
                     traverse_dir(id,full_path)
         traverse_dir(node,directory)
@@ -159,6 +217,62 @@ class Interface:
             runButton['background'] = TOOLBAR
 
         ########################## BUTTONS ###########################
+        def readFile(file, i=1):
+            try:
+                line=file.readline()
+                codingArea.insert(END,line)
+                readFile(file, i+1)
+            except:
+                file.close()
+
+        def saveFile(file, i=0):
+            try:
+                file.write(codingArea.get("1.0",END))
+            except:
+                file.close()        
+
+        def browseFiles():
+            global filename
+            filename = filedialog.askopenfilename(initialdir = "/",
+                                          title = "Select a File",
+                                          filetypes = (("Tagaplate",
+                                                        "*.tpl*"),))
+            if filename != "":
+                codingArea.delete('1.0', END)
+                openedFile["text"]=filename.split('/')[-1]+" - TagaPlate"
+                file=open(filename,"r+")
+                readFile(file)
+        
+        def saveChanges():
+            try:
+                file=open(filename,"r+")
+                saveFile(file)
+            except:
+                saveAs()
+
+        def saveAs():
+            global filename
+            files = [('Tagaplate', '*.tpl')]
+            file = filedialog.asksaveasfile(filetypes = files, defaultextension = files)
+            try:
+                filename=file.name
+                openedFile["text"]=filename.split('/')[-1]+" - TagaPlate"
+                saveFile(file)
+            except:
+                pass
+
+        def onDoubleClick(e):
+            item = folderTree.selection()[0]
+            codingArea.delete('1.0', END)
+            try:
+                filename = "".join(folderTree.item(item,"tags"))
+                openedFile["text"]=folderTree.item(item,"text")+" - TagaPlate"
+                file=open(filename,"r+")
+                readFile(file)
+            except:
+                codingArea.insert(END,"Error al abrir archivo")
+
+        folderTree.bind("<Double-1>", onDoubleClick)
 
         exitImage = cargar_img("quitImage.png")
         exitButton = Button(self.ventana, image=exitImage, width=MENUHEIGHT, height=MENUHEIGHT-2, border=0, bg=MENUBAR)
@@ -168,19 +282,19 @@ class Interface:
         exitButton.bind('<Button-1>', close)
 
         openImage = cargar_img("openImage.png")
-        openButton = Button(toolbar, image=openImage, width=TOOLBWIDTH-2, height=TOOLBWIDTH, border=0, bg=TOOLBAR)
+        openButton = Button(toolbar, image=openImage, width=TOOLBWIDTH-2, height=TOOLBWIDTH, border=0, bg=TOOLBAR, command=browseFiles)
         openButton.place(x=0,y=0, anchor=NW)
         openButton.bind("<Enter>", on_enterOpen)
         openButton.bind("<Leave>", on_leaveOpen)
 
         saveImage = cargar_img("saveImage.png")
-        saveButton = Button(toolbar, image=saveImage, width=TOOLBWIDTH-2, height=TOOLBWIDTH, border=0, bg=TOOLBAR)
+        saveButton = Button(toolbar, image=saveImage, width=TOOLBWIDTH-2, height=TOOLBWIDTH, border=0, bg=TOOLBAR, command=saveChanges)
         saveButton.place(x=0,y=1*BUTTONGRID, anchor=NW)
         saveButton.bind("<Enter>", on_enterSave)
         saveButton.bind("<Leave>", on_leaveSave)
 
         saveAsImage = cargar_img("saveAsImage.png")
-        saveAsButton = Button(toolbar, image=saveAsImage, width=TOOLBWIDTH-2, height=TOOLBWIDTH, border=0, bg=TOOLBAR)
+        saveAsButton = Button(toolbar, image=saveAsImage, width=TOOLBWIDTH-2, height=TOOLBWIDTH, border=0, bg=TOOLBAR, command=saveAs)
         saveAsButton.place(x=0,y=2*BUTTONGRID, anchor=NW)
         saveAsButton.bind("<Enter>", on_enterSaveAs)
         saveAsButton.bind("<Leave>", on_leaveSaveAs)
@@ -201,21 +315,19 @@ class Interface:
         def multipleyView(*args):
             codingArea.yview(*args)
             numbArea.yview(*args)
+        yscrollCode = ttk.Scrollbar(self.ventana, orient="vertical")
+        xscrollCode = ttk.Scrollbar(self.ventana, orient="horizontal")
+        yscrollCode.place(x=WIDTH, y=MENUHEIGHT, anchor=NE, height=HEIGHT-MENUHEIGHT-CONHEIGHT-13)
+        xscrollCode.place(x=TOOLBWIDTH+FOLDERWIDTH+NUMBERSWIDTH, y=HEIGHT-CONHEIGHT+1, anchor=SW, width=WIDTH-TOOLBWIDTH-FOLDERWIDTH-NUMBERSWIDTH+2)
 
-
-        yscrollCode = Scrollbar(mainPanel, orient="vertical")
-        xscrollCode = Scrollbar(mainPanel, orient="horizontal")
-        yscrollCode.place(x=WIDTH-TOOLBWIDTH-FOLDERWIDTH-NUMBERSWIDTH, y=0, anchor=NE, height=HEIGHT-MENUHEIGHT-15)
-        xscrollCode.place(x=0, y=HEIGHT-MENUHEIGHT, anchor=SW, width=WIDTH-TOOLBWIDTH-FOLDERWIDTH-NUMBERSWIDTH)
-
-        codingArea= Text(mainPanel, background=BACKGROUND, highlightthickness=0, height=31, width=110, highlightcolor=BACKGROUND, fg= 'white', insertbackground='white', yscrollcommand=yscrollCode.set, xscrollcommand=xscrollCode.set, wrap="none")
+        codingArea= Text(mainPanel, background=BACKGROUND, highlightthickness=0, height=20, width=110, highlightcolor=BACKGROUND, fg= 'white', insertbackground='white', yscrollcommand=yscrollCode.set, xscrollcommand=xscrollCode.set, wrap="none")
         codingArea.place(x=-1,y=-1)
         codingArea.config(spacing1=SPACING)    # Spacing above the first line in a block of text
         font = tkfont.Font(font=codingArea['font'])
         tab_size = font.measure('    ')
         codingArea.config(tabs=tab_size)
         
-        numbArea= Text(numBar, background=BACKGROUND, width=NUMBERSWIDTH, height=HEIGHT-MENUHEIGHT, highlightthickness=0, highlightcolor=BACKGROUND, fg= LIGHTERBG, yscrollcommand=yscrollCode.set, wrap="none")
+        numbArea= Text(numBar, background=BACKGROUND, width=110, height=20, highlightthickness=0, highlightcolor=BACKGROUND, fg= LIGHTERBG, yscrollcommand=yscrollCode.set, wrap="none", borderwidth=0)
         numbArea.place(x=-1,y=-1)
         numbArea.config(spacing1=SPACING)    # Spacing above the first line in a block of text
         numbArea.config(state=DISABLED)
@@ -237,8 +349,32 @@ class Interface:
 
         codingArea.bind("<KeyRelease>", newCodeLine)
 
+        ########################## CONSOLA ###########################
+
+        terminalLabel=Label(consolePanel, font=fnt, fg="#dedede", bg=BACKGROUND, text="Terminal")
+        terminalLabel.place(x=5,y=5)
+        fontToUnderline = tkfont.Font(terminalLabel, terminalLabel.cget("font"))
+        fontToUnderline.configure(underline = True)
+        terminalLabel.configure(font=fontToUnderline)
+
+        yscrollConsole = ttk.Scrollbar(self.ventana, orient="vertical")
+        xscrollConsole = ttk.Scrollbar(self.ventana, orient="horizontal")
+        yscrollConsole.place(x=WIDTH, y=HEIGHT-CONHEIGHT, anchor=NE, height=CONHEIGHT-13)
+        xscrollConsole.place(x=TOOLBWIDTH+FOLDERWIDTH, y=HEIGHT+1, anchor=SW, width=WIDTH-TOOLBWIDTH-FOLDERWIDTH+2)
+
+        consoleArea= Text(consolePanel, background=BACKGROUND, highlightthickness=0, height=8, width=116, highlightcolor=BACKGROUND, fg= 'white', insertbackground='white', yscrollcommand=yscrollConsole.set, xscrollcommand=xscrollConsole.set, wrap="none", borderwidth=0)
+        consoleArea.place(x=-1,y=30)
+        consoleArea.config(spacing1=SPACING)    # Spacing above the first line in a block of text
+        font = tkfont.Font(font=consoleArea['font'])
+        tab_size = font.measure('    ')
+        consoleArea.config(tabs=tab_size)
+        consoleArea.insert(END,os.getcwd())
+        consoleArea.config(state=DISABLED)
+
+        xscrollConsole.config(command=consoleArea.xview)
+        yscrollConsole.config(command=consoleArea.yview)
+
         #self.ventana.protocol("WM_DELETE_WINDOW",1) 
         self.ventana.mainloop()
-
 
 Interface() 
